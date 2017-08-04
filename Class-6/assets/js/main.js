0c4e02648c725c250c611ddf1ab2a70e201276ae
Erfansgit <erfankhan7104@gmail.com>
2017-08-04 17:56:31 +0600

    Class-6

---
+++ Class-6/assets/js/main.js
@@ -0,0 +1,46 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".case-studies-carousel").owlCarousel({
+            loop: true,
+            margin: 30,           
+            nav: false,        
+            dots: true,     
+            items : 3, 
+            lazyLoad:true,
+            
+        });
+        
+        
+        $(".testimonial-carousel").owlCarousel({
+            loop: true,
+            margin: 30,           
+            nav: false,        
+            dots: true,
+            items : 1, 
+        });
+        
+        $(".logo-carousel").owlCarousel({
+            loop: true,
+            margin: 30,           
+            nav: false,        
+            dots: false,     
+            items : 6, 
+            
+        });
+        
+        
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
